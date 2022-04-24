568d712cd0c6564b7dbcd321d0adc314b36cd94d
David Ralph <me@davidjcralph.co.uk>
2022-04-24 11:53:59 +0100

    auto role etc

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,5 +1,6 @@
 module.exports = async (client, member) => {
     member.guild.channels.cache
       .find((channel) => channel.id === client.config.welcome_channel)
-      .send(`**${member.user.tag}** (${member.user.id}) just joined the server! <:aimainawave:564363881942876165> (Member #${member.guild.memberCount})`);
+      .send(`**${member.user.tag}** (<@${member.user.id}>) just joined the server! <:aimainawave:564363881942876165> (Member #${member.guild.memberCount})`);
+    member.roles.add(client.config.welcome_role);
 };
